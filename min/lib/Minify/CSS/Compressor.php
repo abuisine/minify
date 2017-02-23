908b8a3006933e61b345358997f802acb933815a
Alexandre Buisine <alexandrejabuisine@gmail.com>
2017-02-23 18:23:02 +0100

    remove css new lines

--- min/lib/Minify/CSS/Compressor.php
+++ min/lib/Minify/CSS/Compressor.php
@@ -148,6 +148,8 @@ class Minify_CSS_Compressor {
         
         // prevent triggering IE6 bug: http://www.crankygeek.com/ie6pebug/
         $css = preg_replace('/:first-l(etter|ine)\\{/', ':first-l$1 {', $css);
+
+        $css = preg_replace('/\\n+/', " ", $css);
             
         return trim($css);
     }
